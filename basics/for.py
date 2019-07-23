5cecd1738a2ea9e15bafa9997a9b1f58454cfb33
mukesh.kumar <mukesh.kumar@scandichotels.com>
2019-07-23 15:09:49 +0200

    added some more basic things for the for loop

---
+++ basics/for.py
@@ -0,0 +1,13 @@
+# for loop
+
+listOfTupple = [("Mukesh",39), ("Sarab", 37), ("Vikas",36),("Saurabh",40)]
+
+for name,age in listOfTupple:
+    print(f"{name} is {age} years old")
+
+
+for friend in friends:
+    print("Hello my Friend: " + friend)
+
+
+
--- basics/while.py
+++ basics/while.py
@@ -1,5 +1,7 @@
-user_input = input('Enter q or p: ')
 
+# while loop
+
+user_input = input('Enter q or p: ')
 # Now we must repeat until they type 'q':
 while user_input != 'q':
     # Inside our loop, check if they typed 'p'. If they did, print "Hello"
@@ -7,3 +9,9 @@ while user_input != 'q':
         print("Hello : "+user_input)
     # Now we must ask the user for their input again—otherwise we would be in an infinite loop!
     user_input = input('Enter q or p: ')
+
+
+
+
+
+
