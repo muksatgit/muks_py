4a25687033c232567281f5e537376a4bd2f1b38c
mukesh.kumar <mukesh.kumar@scandichotels.com>
2019-07-23 14:28:24 +0200

    while loop with if condition inside

--- loop.py
+++ loop.py
@@ -1,1 +1,9 @@
-userOption = input("Please Select an option, if you want to print something enter 'p' and if your want to quite teh program enter 'q' :\n")
+user_input = input('Enter q or p: ')
+
+# Now we must repeat until they type 'q':
+while user_input != 'q':
+    # Inside our loop, check if they typed 'p'. If they did, print "Hello"
+    if user_input == 'p':
+        print("Hello : "+user_input)
+    # Now we must ask the user for their input again—otherwise we would be in an infinite loop!
+    user_input = input('Enter q or p: ')
