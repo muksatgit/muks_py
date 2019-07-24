1f44ee1136120740c113ca485c9d180e4f18f1bb
mukesh.kumar <mukesh.kumar@scandichotels.com>
2019-07-24 13:56:23 +0200

    break and continue

--- basics/for.py
+++ basics/for.py
@@ -1,13 +1,24 @@
 # for loop
 
 listOfTupple = [("Mukesh",39), ("Sarab", 37), ("Vikas",36),("Saurabh",40)]
+listOfDictionary = {"Mukesh":39, "Sarab": 37, "Vikas":36,"Saurabh":40}
 
+print("----------------------------------")
 for name,age in listOfTupple:
     print(f"{name} is {age} years old")
+print("----------------------------------")
+for name in listOfDictionary:
+    print(name)
 
+print("----------------------------------")
+for iName, iAge in listOfDictionary.items():
+    print(f"{iName} is {iAge} years old")
 
-for friend in friends:
-    print("Hello my Friend: " + friend)
+print("----------------------------------")
+
+for i in range(10):
+  print(i)
+
 
 
 
---
+++ basics/listComprehensions.py
@@ -0,0 +1,13 @@
+names = ["Mukesh","Sachin","NIKHIL"]
+name_lower = [name.lower() for name in names]
+
+
+for name in names:
+    print(name)
+print("----------------")
+
+for name in name_lower:
+    print(name)
+
+
+
