249c98c5d9c2120d454f0c0365614d395ef92682
mukesh.kumar <mukesh.kumar@scandichotels.com>
2019-08-01 17:06:15 +0200

    errors and file operation

---
+++ Files/File.py
@@ -0,0 +1,19 @@
+my_file = open('data.txt','r')
+file_content = my_file.read()
+my_file.close()
+
+print(file_content)
+
+
+name = input("Enter Your Name: ")
+my_file_writing = open('data.txt','w')
+my_file_writing.write(name)
+my_file_writing.close()
+
+
+
+my_file = open('data.txt','r')
+file_content = my_file.read()
+my_file.close()
+
+print(file_content)
